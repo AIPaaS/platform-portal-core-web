c6a5e4cfeb4bed08e8368508c3ba01bf54ad693b
wanwb <wanwb@asiainfo.com>
2016-03-04 09:41:38 +0800

    添加任务运行监控模块

---
+++ src/main/java/com/aic/paas/web/dep/mvc/PcAppMonitorMvc.java
@@ -0,0 +1,11 @@
+package com.aic.paas.web.dep.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping("/dep/appmonitor")
+public class PcAppMonitorMvc {
+
+}
---
+++ src/main/java/com/aic/paas/web/dep/peer/impl/PcAppMonitorPeerImpl.java
@@ -0,0 +1,7 @@
+package com.aic.paas.web.dep.peer.impl;
+
+import com.aic.paas.web.dep.peer.PcAppMonitorPeer;
+
+public class PcAppMonitorPeerImpl implements PcAppMonitorPeer {
+
+}
