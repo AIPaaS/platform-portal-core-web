b2f039d113e448467b406fdfcf85920cc26cd94a
liwx3 <liwx3@asiainfo.com>
2016-03-08 10:50:01 +0800

    modify

--- src/main/java/com/aic/paas/web/dep/bean/PcServiceInfo.java
+++ src/main/java/com/aic/paas/web/dep/bean/PcServiceInfo.java
@@ -14,6 +14,14 @@ public class PcServiceInfo implements Serializable {
 	/** 服务参数 **/
 	private List<PcKvPair> params;
 
+	/**提供者 应用名称*/
+	private String provider;
+	/**引用者 容器ID （调用该服务的容器） */
+	private List<Long> consumerIds;
+	/**引用者 容器名称*/
+	private List<String> consumers;
+	/**引用者 容器名称  描述*/
+	private String consumerDes;
 
 	public PcService getSvc() {
 		return svc;
@@ -33,7 +41,46 @@ public class PcServiceInfo implements Serializable {
 	public void setParams(List<PcKvPair> params) {
 		this.params = params;
 	}
-	
+
+
+	public String getProvider() {
+		return provider;
+	}
+
+
+	public void setProvider(String provider) {
+		this.provider = provider;
+	}
+
+
+	public List<Long> getConsumerIds() {
+		return consumerIds;
+	}
+
+
+	public void setConsumerIds(List<Long> consumerIds) {
+		this.consumerIds = consumerIds;
+	}
+
+
+	public List<String> getConsumers() {
+		return consumers;
+	}
+
+
+	public void setConsumers(List<String> consumers) {
+		this.consumers = consumers;
+	}
+
+
+	public String getConsumerDes() {
+		return consumerDes;
+	}
+
+
+	public void setConsumerDes(String consumerDes) {
+		this.consumerDes = consumerDes;
+	}
 	
 	
 	
