f54cfc51521627f0c67d89c7a128f73e97c6374b
zhangqiang7 <zhangqiang7@aisainfo.com>
2016-03-15 10:44:07 +0800

    取消注释

--- src/main/webapp/dep/js/PcAppImageForm_Image.js
+++ src/main/webapp/dep/js/PcAppImageForm_Image.js
@@ -329,16 +329,14 @@ function submitForm(cb){
 	if(bean.instanceCount==0) {CC.showMsg({msg:"容器数不可以为0"}); return;}
 	if(ResidueRes.cpuCount < 0) {CC.showMsg({msg:"当前网络区域<font color='red'>CPU</font>资源<font color='red'>不充足</font>，请调整CPU大小!"}); return;}
 	if(ResidueRes.memSize < 0) {CC.showMsg({msg:"当前网络区域<font color='red'>内存</font>资源<font color='red'>不充足</font>，请调整内存大小!"}); return;}
-	//if(ResidueRes.diskSize < 0) {CC.showMsg({msg:"当前网络区域<font color='red'>存储</font>资源<font color='red'>不充足</font>，请调整存储大小!"}); return;}
+	if(ResidueRes.diskSize < 0) {CC.showMsg({msg:"当前网络区域<font color='red'>存储</font>资源<font color='red'>不充足</font>，请调整存储大小!"}); return;}
 	if(CU.isEmpty(ResidueRes.diskSize)) ResidueRes.diskSize = 0;
-	
 	bean.appId = AppId;
 	bean.appVnoId = AppVnoId;
 	if(!CU.isEmpty(AppImageId)) bean.id = AppImageId;
 	
 	RS.ajax({url:"/dep/appimage/saveAppImage",ps:bean,cb:function(rs) {
 		AppImageId = rs;
-		
 		$("#btn_save").prop("title", "保存成功");
 		$("#btn_save").tooltip("show");
 		setTimeout(function(){
