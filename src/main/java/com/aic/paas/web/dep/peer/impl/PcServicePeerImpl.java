865bc8af15331512a366b307084863aa6117f0d6
马建华 <smilence1986@gmail.com>
2016-03-16 14:30:23 +0800

    1. 提测的功能说明：
        1.1 外部服务新增时，注册到consul
        1.2 外部服务修改时，consul服务更新
        1.3 外部服务删除时，consul删除服务
    
      2. 代码分支说明:
    
        2.1 用户门户
        2.2 用户门户的后场服务
        2.3 consul的后场服务
    
        以上工程代码均使用 develop 分支构建部署，github地址：
        https://github.com/AIPaaS/PaaS-FE-Platform-Portal.git
        https://github.com/AIPaaS/PaaS-BE-Platform-MGMT.git
        https://github.com/AIPaaS/PaaS-BE-AM.git
        
    
      3. 配置信息说明：
    
        用户门户
    的配置：/PaaS-FE-Platform-Portal/src/test/resources/project.properties
        需要修改dubbox.rest.root.besrv = rest://127.0.0.1:20880/cpaas/为服务器地址
       
    
      4. 数据库脚本：
    
        无  
    
      5. 第三方软件列表：
    
        需要安装consul

--- src/main/java/com/aic/paas/web/dep/peer/impl/PcServicePeerImpl.java
+++ src/main/java/com/aic/paas/web/dep/peer/impl/PcServicePeerImpl.java
@@ -179,7 +179,7 @@ public class PcServicePeerImpl implements PcServicePeer {
 			if(record.getUserName() != null) BinaryUtils.checkEmpty(record.getUserName(), "record.userName");
 			if(record.getAppImageId() != null) BinaryUtils.checkEmpty(record.getAppImageId(), "record.appImageId");
 		}
-//		 registerToConsul(record);
+	    registerToConsul(record);
 		return serviceSvc.saveOrUpdate(record);
 	}
 	
@@ -224,7 +224,7 @@ public class PcServicePeerImpl implements PcServicePeer {
 		
 		PcService record = queryById(svcType, id);
 		if(record != null) {
-//			deleteSrvFromConsul(record);
+			deleteSrvFromConsul(record);
 			return serviceSvc.removeById(id);
 		}
 		return 0;
