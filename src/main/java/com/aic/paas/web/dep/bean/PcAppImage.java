7c90da68220f38fec1325c2cefed7a6afa148a0a
zhangqiang7 <zhangqiang7@aisainfo.com>
2016-03-18 21:02:31 +0800

    bean

--- src/main/java/com/aic/paas/web/dep/bean/PcAppImage.java
+++ src/main/java/com/aic/paas/web/dep/bean/PcAppImage.java
@@ -32,12 +32,6 @@ public class PcAppImage implements EntityBean {
 
 
 	/**
-	 * mapping-field: 镜像[IMAGE]
-	 */
-	private String image;
-
-
-	/**
 	 * mapping-field: 应用ID[APP_ID]
 	 */
 	private Long appId;
@@ -89,6 +83,12 @@ public class PcAppImage implements EntityBean {
 
 
 	/**
+	 * mapping-field: CPU总数[CPU_TOTAL]
+	 */
+	private Integer cpuTotal;
+
+
+	/**
 	 * mapping-field: 内存大小[MEM_SIZE]
 	 * 单位：M
 	 */
@@ -96,10 +96,22 @@ public class PcAppImage implements EntityBean {
 
 
 	/**
+	 * mapping-field: 内存总数[MEM_TOTAL]
+	 */
+	private Long memTotal;
+
+
+	/**
 	 * mapping-field: 存储大小[DISK_SIZE]
 	 * 单位：M
 	 */
 	private Long diskSize;
+
+
+	/**
+	 * mapping-field: 存储总数[DISK_TOTAL]
+	 */
+	private Long diskTotal;
 
 
 	/**
@@ -295,14 +307,6 @@ public class PcAppImage implements EntityBean {
 	}
 
 
-	public String getImage() {
-		return this.image;
-	}
-	public void setImage(String image) {
-		this.image = image;
-	}
-
-
 	public Long getAppId() {
 		return this.appId;
 	}
@@ -367,6 +371,14 @@ public class PcAppImage implements EntityBean {
 	}
 
 
+	public Integer getCpuTotal() {
+		return this.cpuTotal;
+	}
+	public void setCpuTotal(Integer cpuTotal) {
+		this.cpuTotal = cpuTotal;
+	}
+
+
 	public Long getMemSize() {
 		return this.memSize;
 	}
@@ -375,11 +387,27 @@ public class PcAppImage implements EntityBean {
 	}
 
 
+	public Long getMemTotal() {
+		return this.memTotal;
+	}
+	public void setMemTotal(Long memTotal) {
+		this.memTotal = memTotal;
+	}
+
+
 	public Long getDiskSize() {
 		return this.diskSize;
 	}
 	public void setDiskSize(Long diskSize) {
 		this.diskSize = diskSize;
+	}
+
+
+	public Long getDiskTotal() {
+		return this.diskTotal;
+	}
+	public void setDiskTotal(Long diskTotal) {
+		this.diskTotal = diskTotal;
 	}
 
 
