f077b5fa2b58083811f329fd22f7ed5e6adb7b12
liwx3 <liwx3@asiainfo.com>
2016-03-21 19:54:11 +0800

    modify app_access

--- src/main/java/com/aic/paas/web/dep/mvc/PcAppAccessMvc.java
+++ src/main/java/com/aic/paas/web/dep/mvc/PcAppAccessMvc.java
@@ -54,11 +54,11 @@ public class PcAppAccessMvc {
 	//保存
 	@RequestMapping("/saveOrUpdate")
 	public void saveOrUpdate(HttpServletRequest request,HttpServletResponse response, PcAppAccess record) {
-		Long id = appAccessPeer.saveOrUpdate(record);
-		
 		HttpClient client = HttpClient.getInstance(taskRoot);
 		String json = client.request("/interface/dep/appaccess/add?record=" + JSON.toString(record));
 		json = ControllerUtils.toRemoteJsonObject(json, String.class);
+		
+		Long id = appAccessPeer.saveOrUpdate(record);
 		ControllerUtils.returnJson(request, response, id);
 	}
 	//删除
